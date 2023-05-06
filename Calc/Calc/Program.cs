00ba1e4b538a21ad65c2fbd52109dfee44e38481
Oleg Potkin <olpotkin@gmail.com>
2023-05-06 15:10:21 +0300

    feat: Convert code to internal class representation

--- Calc/Calc/Program.cs
+++ Calc/Calc/Program.cs
@@ -2,19 +2,25 @@
 /// First C# console application
 /// ==============================
 
-Console.WriteLine("Insert first number: ");
-string str = Console.ReadLine();
-int x = Int32.Parse(str);
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        Console.WriteLine("Insert first number: ");
+        string str = Console.ReadLine();
+        int x = int.Parse(str);
 
-Console.WriteLine("Insert second number: ");
-str = Console.ReadLine();
-int y = Int32.Parse(str);
+        Console.WriteLine("Insert second number: ");
+        str = Console.ReadLine();
+        int y = int.Parse(str);
 
-// Subtraction
-Console.WriteLine($"Subtraction result: {x - y}");
-// Addition
-Console.WriteLine($"Addition result: {x + y}");
-// Multiplication
-Console.WriteLine($"Multiplication result: {x * y}");
-// Division
-Console.WriteLine($"Division result: {x / y}");
+        // Subtraction
+        Console.WriteLine($"Subtraction result: {x - y}");
+        // Addition
+        Console.WriteLine($"Addition result: {x + y}");
+        // Multiplication
+        Console.WriteLine($"Multiplication result: {x * y}");
+        // Division
+        Console.WriteLine($"Division result: {x / y}");
+    }
+}
