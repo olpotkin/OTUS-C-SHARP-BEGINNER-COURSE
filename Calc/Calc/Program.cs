735c70d381ad29d128982e0d096d7091d7c3ef3e
Oleg Potkin <olpotkin@gmail.com>
2023-05-06 14:56:32 +0300

    feat: Add basic math operations with integers
    Subtraction, Addition, Multiplication and Division with integers in Console app

--- Calc/Calc/Program.cs
+++ Calc/Calc/Program.cs
@@ -1,3 +1,20 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿/// ==============================
+/// First C# console application
+/// ==============================
 
+Console.WriteLine("Insert first number: ");
+string str = Console.ReadLine();
+int x = Int32.Parse(str);
+
+Console.WriteLine("Insert second number: ");
+str = Console.ReadLine();
+int y = Int32.Parse(str);
+
+// Subtraction
+Console.WriteLine($"Subtraction result: {x - y}");
+// Addition
+Console.WriteLine($"Addition result: {x + y}");
+// Multiplication
+Console.WriteLine($"Multiplication result: {x * y}");
+// Division
+Console.WriteLine($"Division result: {x / y}");
