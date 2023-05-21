46487fa71dcc7e90802642591d56558372848b9b
Oleg Potkin <olpotkin@gmail.com>
2023-05-21 18:46:14 +0300

    feat: Check if input is correct

--- Calc/Calc/Program.cs
+++ Calc/Calc/Program.cs
@@ -6,14 +6,33 @@ internal class Program
 {
     private static void Main(string[] args)
     {
-        int x = GetNumber("Insert first number: ");
-        int y = GetNumber("Insert second number: ");
+        do
+        {
+            int x = GetNumber("Insert first number: ");
+            int y = GetNumber("Insert second number: ");
 
-        Console.WriteLine("Please, choose the command: +, -, *, /, min, max");
-        string cmd = Console.ReadLine();
+            Console.WriteLine("Please, choose the command: +, -, *, /, min, max");
+            string cmd = Console.ReadLine();
 
-        int result = GetResult(x, y, cmd);
-        Console.WriteLine(result);
+            int result = GetResult(x, y, cmd);
+            Console.WriteLine(result);
+
+        } while (AskContinue());
+    }
+
+    private static bool AskContinue()
+    {
+        while (true)
+        {
+            Console.WriteLine("Do you want to continue? Y/N");
+            var status = Console.ReadLine();
+            if (status == "Y" || status == "y")
+                return true;
+            else if (status == "N" || status == "n")
+                return false;
+            else
+                Console.WriteLine("Operation is not correct. Insert Y or N");
+        }
     }
 
     private static int GetResult(int x, int y, string cmd)
@@ -50,9 +69,20 @@ internal class Program
     private static int GetNumber(string text)
     {
         Console.WriteLine(text);
-        string str = Console.ReadLine();
 
-        return int.Parse(str);
+        while (true)
+        {
+            string str = Console.ReadLine();
+            int result;
+
+            // Check if user input is correct
+            if (Int32.TryParse(str, out result))
+                return result;
+            else
+                Console.WriteLine("Wrong format. Try again.");
+        }
+
+
     }
 
     // Max value: x or y
