28a35931743e08c428d0b9f1cd7fab2bbc73bdf5
Oleg Potkin <olpotkin@gmail.com>
2023-06-04 14:24:35 +0300

    ref: Class formatting alignment

--- Tetris/Tetris/Point.cs
+++ Tetris/Tetris/Point.cs
@@ -3,12 +3,12 @@ namespace Tetris
 {
 	class Point
 	{
-		public int x;
-		public int y;
+        public int x;
+        public int y;
         public char c;
 
-		public void Draw()
-		{
+        public void Draw()
+        {
             Console.SetCursorPosition(x, y);
             Console.Write(c);
         }
