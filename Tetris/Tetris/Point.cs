8f02bd454e36f44c8049c01f3591a518710d50fa
Oleg Potkin <olpotkin@gmail.com>
2023-06-04 14:15:13 +0300

    ref: Add class Point

---
+++ Tetris/Tetris/Point.cs
@@ -0,0 +1,17 @@
+﻿using System;
+namespace Tetris
+{
+	class Point
+	{
+		public int x;
+		public int y;
+        public char c;
+
+		public void Draw()
+		{
+            Console.SetCursorPosition(x, y);
+            Console.Write(c);
+        }
+	}
+}
+
--- Tetris/Tetris/Program.cs
+++ Tetris/Tetris/Program.cs
@@ -1,29 +1,25 @@
-﻿internal class Program
+﻿using Tetris;
+
+class Program
 {
-    private static void Main(string[] args)
+    static void Main(string[] args)
     {
         // Only for Windows Platform:
         //Console.SetWindowSize(40, 30);
         //Console.SetBufferSize(40, 30);
 
-        int x1 = 2;
-        int y1 = 3;
-        char c1 = '*';
-        Draw(x1, y1, c1);
+        Point p1 = new Point();
+        p1.x = 2;
+        p1.y = 3;
+        p1.c = '*';
+        p1.Draw();
 
-        int x2 = 4;
-        int y2 = 5;
-        char c2 = '#';
-        Draw(x2, y2, c2);
+        Point p2 = new Point();
+        p2.x = 4;
+        p2.y = 5;
+        p2.c = '*';
+        p2.Draw();
 
         Console.ReadLine();
     }
-
-
-    static void Draw(int x, int y, char c)
-    {
-        Console.SetCursorPosition(x, y);
-        Console.Write(c);
-    }
-
 }
