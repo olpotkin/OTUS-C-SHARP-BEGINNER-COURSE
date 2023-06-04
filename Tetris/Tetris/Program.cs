6e8a50ccfdea7ecdc12caf39ecb2f908ff4edc99
Oleg Potkin <olpotkin@gmail.com>
2023-06-04 13:51:01 +0300

    feat: Add ability to set target coordinates to draw character on the console

--- Tetris/Tetris/Program.cs
+++ Tetris/Tetris/Program.cs
@@ -1,7 +1,29 @@
-﻿internal class Tetris
+﻿internal class Program
 {
     private static void Main(string[] args)
     {
-        Console.WriteLine("Hello, Tetris!");
+        // Only for Windows Platform:
+        //Console.SetWindowSize(40, 30);
+        //Console.SetBufferSize(40, 30);
+
+        int x1 = 2;
+        int y1 = 3;
+        char c1 = '*';
+        Draw(x1, y1, c1);
+
+        int x2 = 4;
+        int y2 = 5;
+        char c2 = '#';
+        Draw(x2, y2, c2);
+
+        Console.ReadLine();
     }
+
+
+    static void Draw(int x, int y, char c)
+    {
+        Console.SetCursorPosition(x, y);
+        Console.Write(c);
+    }
+
 }
