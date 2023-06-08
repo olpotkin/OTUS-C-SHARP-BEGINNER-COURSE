baa2f79d9888376725a453305dc4712d0f3ab7df
Oleg Potkin <olpotkin@gmail.com>
2023-06-08 22:38:31 +0300

    feat: Add class Square

--- Tetris/Tetris/Program.cs
+++ Tetris/Tetris/Program.cs
@@ -8,6 +8,9 @@
             //Console.SetWindowSize(40, 30);
             //Console.SetBufferSize(40, 30);
 
+            Square s = new Square(2, 5, '*');
+            s.Draw();
+
             Point p1 = new Point(2, 3, '*');
             p1.Draw();
 
