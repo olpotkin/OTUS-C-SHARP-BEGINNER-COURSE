cf445ee16b6c8722e64e924b0056c608d1aff23d
Oleg Potkin <olpotkin@gmail.com>
2023-06-12 15:47:01 +0300

    feat: Add base class Figure

--- Tetris/Tetris/Stick.cs
+++ Tetris/Tetris/Stick.cs
@@ -2,24 +2,14 @@
 
 namespace Tetris
 {
-    public class Stick
+    public class Stick : Figure
     {
-        Point[] points = new Point[4];
-
         public Stick(int x, int y, char sym)
         {
             points[0] = new Point(x, y + 1, sym);
             points[1] = new Point(x, y + 2, sym);
             points[2] = new Point(x, y + 3, sym);
             points[3] = new Point(x, y + 4, sym);
-        }
-
-        public void Draw()
-        {
-            foreach (Point p in points)
-            {
-                p.Draw();
-            }
         }
     }
 }
