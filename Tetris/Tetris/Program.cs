ecb1cd18bd4400e3842c3a073caab8bd9cffa4a9
Oleg Potkin <olpotkin@gmail.com>
2023-06-24 12:42:42 +0300

    feat: Add Processing for falling figures

--- Tetris/Tetris/Program.cs
+++ Tetris/Tetris/Program.cs
@@ -4,13 +4,15 @@ namespace Tetris
 {
     class Program
     {
+        static FigureGenerator generator;
+
         static void Main(string[] args)
         {
             // Only for Windows Platform:
             //Console.SetWindowSize(Field.Width, Field.Height);
             //Console.SetBufferSize(Field.Width, Field.Height);
 
-            FigureGenerator generator = new FigureGenerator(20, 0, '*');
+            generator = new FigureGenerator(20, 0, '*');
             Figure currentFigure = generator.GetNewFigure();
 
             while (true)
@@ -19,29 +21,41 @@ namespace Tetris
                 {
                     // var automatically replaces ConsoleKeyInfo
                     var key = Console.ReadKey();
-
-                    HandleKey(currentFigure, key);
+                    var result = HandleKey(currentFigure, key.Key);
+                    ProcessResult(result, ref currentFigure);
                 }
             }
         }
 
-        private static void HandleKey(Figure currentFigure, ConsoleKeyInfo key)
+
+        private static bool ProcessResult(Result result, ref Figure currentFigure)
         {
-            switch (key.Key)
+            if (result == Result.HEAP_STRIKE || result == Result.DOWN_BORDER_STRIKE)
+            {
+                Field.AddFigure(currentFigure);
+                currentFigure = generator.GetNewFigure();
+                return true;
+            }
+            else
+                return false;
+        }
+
+
+        private static Result HandleKey(Figure f, ConsoleKey key)
+        {
+            switch (key)
             {
                 case ConsoleKey.LeftArrow:
-                    currentFigure.TryMove(Direction.LEFT);
-                    break;
+                    return f.TryMove(Direction.LEFT);
                 case ConsoleKey.RightArrow:
-                    currentFigure.TryMove(Direction.RIGHT);
-                    break;
+                    return f.TryMove(Direction.RIGHT);
                 case ConsoleKey.DownArrow:
-                    currentFigure.TryMove(Direction.DOWN);
-                    break;
+                    return f.TryMove(Direction.DOWN);
                 case ConsoleKey.Spacebar:
-                    currentFigure.TryRotate();
-                    break;
+                    return f.TryRotate();
             }
+            return Result.SUCCESS;
         }
+
     }
 }
